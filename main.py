caf6ed5dbc7c96a485390b06d32024a37e1229bf
mario <mariokurwaq337@gmail.com>
2022-02-14 17:46:00 +0400

    Перенес классы пули и персонажа в отдельный файл Теперь все работает едино

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import pygame
 import math
 from pygame.math import Vector2
+from asteroid import Bomb, all_sprites
+import random
+from character import Chacter, Bullet
 
 # Изображение не получится загрузить
 # без предварительной инициализации pygame
@@ -21,143 +24,66 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, *group, x, y, angle):
-        super().__init__(*group)
-        self.source = pygame.transform.scale(load_image("bullet.png"), (25, 10))
-        self.image = pygame.transform.scale(self.source, (25, 10))
-        self.rect = self.image.get_rect()
-        self.rect.centerx = x
-        self.rect.centery = y
-        self.angle = angle
-        offset = Vector2(40, 0).rotate(self.angle)
-        self.accel = Vector2(0.001, 0).rotate(self.angle - 90)
-        self.op_accel = Vector2(-0.002, 0).rotate(self.angle - 90)
-        self.pos = Vector2(self.rect.centerx, self.rect.centery) + offset
-        print(self.pos)
-        self.image = pygame.transform.rotate(self.source, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("asteroid.png")
 
-
-class Chacter(pygame.sprite.Sprite):
     def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
         super().__init__(*group)
-        self.source = pygame.transform.scale(load_image("ship.png"), (200, 200))
-        self.image = pygame.transform.scale(self.source, (200, 200))
+        self.image = Bomb.image
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.centerx = 100
-        self.rect.centery = 100
-        self.rect.centerx = 100
-        self.rect.centery = 100
-        self.angle = 0
-        offset = Vector2(40, 0).rotate(self.angle)
-        self.accel = Vector2(10, 0).rotate(self.angle - 90)
+        self.rect.x = random.randrange(60, width - 100)
+        self.rect.y = random.randrange(61, height - 100)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+        while pygame.sprite.spritecollideany(self, all_spr) != self:
+            self.rect.x = random.randrange(60, width - 100)
+            self.rect.y = random.randrange(61, height - 100)
 
-        self.pos = Vector2(self.rect.centerx, self.rect.centery) + offset
-        # print(self.angle, (self.angle - 90) % 360)
-
-    def rot(self, a):
-        self.angle = (self.angle + a) % 360
-        self.image = pygame.transform.rotate(self.source, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        self.accel = Vector2(0.03, 0).rotate(360 - self.angle - 90)
-        # print(self.angle, (360 - self.angle - 90)
-
-    def update(self, *args):
-        if args[0] == pygame.K_DOWN:
-            self.rect.centerx += 10
-        if args[0] == pygame.K_UP:
-            self.rect.centery -= 10
-        if args[0] == pygame.K_RIGHT:
-            self.angle -= 10
-
-            #   print(self.image.get_rect())
-
-            self.image = pygame.transform.rotate(self.source, self.angle)
-            self.rect = self.image.get_rect(center=self.rect.center)
-
-            # self.rect.center = self.rect.centerx, self.rect.centery
-            # self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.rect.x, self.rect.y)).center)
-        if args[0] == pygame.K_LEFT:
-            self.image = pygame.transform.rotate(self.image, math.pi / 36)
-            self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.rect.x, self.rect.y)).center)
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if self.rect[1] > 600:
+            self.rect[1] = 0
+        if self.rect[0] > 800:
+            self.rect[0] = 0
+        if self.rect[0] < 0:
+            self.rect[0] = 800
+        if self.rect[1] < 0:
+            self.rect[1] = 600
 
 
-v = 1
-move = {pygame.K_LEFT: (+1.25, 0),
-        pygame.K_RIGHT: (-1.25, 0),
-        pygame.K_UP: (0, -v),
-        pygame.K_DOWN: (0, +v),
-        pygame.K_SPACE: (+v, 0)}
+
 
 clock = pygame.time.Clock()
-
 if __name__ == '__main__':
     running = True
+    asteroid_delay = 0
     all_spr = pygame.sprite.Group()
-
-    cur = Chacter(all_spr)
+    for i in range(10):
+        Bomb(all_spr)
+    self = Chacter(all_spr)
     clock = pygame.time.Clock()
-    dir = Vector2(0, 0)
-    vel = Vector2(2.5, 0)
     bullets = pygame.sprite.Group()
     shoot_delay = 0
-    k = 0.3
     while running:
+
         shoot_delay += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        #            if event.type == pygame.KEYDOWN:
-        #                 v = event.type
-        #                 all_spr.update(event.key)
-        for i in move:
 
-            if pygame.key.get_pressed()[i] and (i == pygame.K_DOWN or i == pygame.K_UP):
-                # print(cur.angle)
-
-                dir += cur.accel * k
-                if k < 1:
-                    k += 0.01
-                print(dir)
-                #      print(cur.velocity)
-                screen.fill(pygame.Color("white"))
-                screen.blit(cur.image, cur.rect)
-                if dir.length() > 2.6:
-                    dir.scale_to_length(1.6)
-
-            elif pygame.key.get_pressed()[i] and (i == pygame.K_LEFT or i == pygame.K_RIGHT):
-                cur.rot(move[i][0])
-                k = 0.3
-                screen.fill(pygame.Color("white"))
-                screen.blit(cur.image, cur.rect)
-            elif pygame.key.get_pressed()[i] and (i == pygame.K_SPACE) and shoot_delay > 45:
-
-                print(cur.rect.center)
-                bul = Bullet(bullets, x=cur.rect.center[0], y=cur.rect.center[1], angle=cur.angle)
-                print([i for i in all_spr])
-                print(cur.pos)
-                print(cur.rect.center)
-                shoot_delay = 0
-            #     print(cur.velocity)
-        if dir[0] != 0 or dir[1] != 0:
-            cur.pos += dir  # Add velocity to pos to move the sprite.
-            cur.rect.center = cur.pos  # Update rect coords.
-            dir -= dir * 0.00000005
-        else:
-            dir = [0, 0]
-
-        for j in bullets:
-            j.pos += Vector2(3, 0).rotate(360 - j.angle - 90)
-            j.rect.center = j.pos
-            if -100 < j.pos[0] > width + 100 or -100 < j.pos[1] > height + 100:
-                j.kill()
-                bullets.remove(j)
-
-
+        if pygame.key.get_pressed()[pygame.K_SPACE] and shoot_delay > 45:
+            bul = Bullet(bullets, x=self.rect.center[0], y=self.rect.center[1], angle=self.angle)
+            shoot_delay = 0
         screen.fill((255, 255, 255))
+        all_spr.update()
         all_spr.draw(screen)
         bullets.draw(screen)
+        bullets.update()
+
         pygame.display.flip()
         clock.tick(100)
+
     pygame.quit()
