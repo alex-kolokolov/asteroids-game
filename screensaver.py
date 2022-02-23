2088e51a73eb4abf292221ef7a8814e5ffc70ac4
artem <artem.gr07>
2022-02-23 15:36:29 +0400

    screensaver

---
+++ screensaver.py
@@ -0,0 +1,38 @@
+FPS = 50
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Приветствуем!", "",
+                  "Правила игры",
+                  "1) Вам предстоит сражаться за свой корабль отстреливаясь от астероидов!",
+                  "2) У вас есть три жизни, когда все жизни закончатся вы проиграеие!",
+                  "3) За каждый сбитый астероид вам начисляются очки.!",
+                  "Желаем вам удачи!",
+                  "Нажмите на любую кнопку"]
+
+    fon = pygame.transform.scale(load_image_fon('fon.jpg'), (800, 600))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
